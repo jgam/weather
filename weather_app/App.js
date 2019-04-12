a5b919f5c9020f721cc31833610a6e1484c0e0d2
jgam <jgam@alumni.nd.edu>
2019-04-12 10:22:46 +0900

    weather app

---
+++ weather_app/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator, StatusBar} from 'react-native';
+import Weather from "./weather";
+//import console = require('console');
+
+const API_KEY = "2a21c6eb2cbf24a78d9f4e5c7daeaf06";
+
+export default class App extends React.Component {
+  state = {
+    isLoaded: false,
+  };
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+    position => {
+      this._getWeather(position.coords.latitude, position.coords.longitude);
+    
+      
+      //what is set State here?
+    /*  this.setState({
+        isLoaded: true,
+        error: null
+      })
+    */
+    }//,
+    /*
+    error => {
+      this.setState({
+        error: error
+      })
+      console.log(error);
+    }
+    */
+    )
+  }
+  _getWeather = (lat, lon) => {
+    console.log('here in get weather!');
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+      .then(response => response.json)
+      .then(json =>{
+        console.log(json);
+      });
+  }
+  render() {
+    const { isLoaded, error } = this.state;
+    return (//view component will be returned to necessary component.
+      <View style={styles.container}>
+      <StatusBar barStyle = "dark-content" />
+        {isLoaded ? (<Weather />) : (
+        <View style={styles.loading}>
+          <ActivityIndicator />
+          <Text style={styles.loadingText}>Getting the weather . . !</Text>
+          {error ? <Text style={styles.errorText}>{error}</Text> : null}     
+        </View>
+      )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  errorText:{
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
+  },
+  loading:{
+    flex:1,
+    backgroundColor:"#FDF6AA",
+    justifyContent: 'flex-end',
+    paddingLeft: 25
+  },
+  loadingText:{
+    fontSize: 30,
+    marginBottom: 500
+  }
+});
---
+++ weather_app/weather.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {LinearGradient} from "expo";
+import {Ionicons} from "@expo/vector-icons";
+
+export default class Weather extends Component{
+    render(){
+        return(
+        <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
+        <Text>hello this is sea . . !</Text>
+        <View style={styles.upper}>
+            <Ionicons color="white" size={144} name="ios-rainy"/>
+            <Text style={styles.temp}>35 degrees</Text>
+        </View>
+        <View style={styles.lower}>
+            <Text style={styles.titles}>Raining Like</Text>
+            <Text style={styles.subtitle}>For more info look outside</Text>
+        </View>
+        </LinearGradient>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    upper:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center",
+        backgroundColor: "transparent"
+    },
+    temp:{
+        fontSize: 45,
+        backgroundColor: "transparent",
+        color: "white",
+        marginTop: 10
+    },
+    lower:{
+        flex: 1,
+        alignItems: "flex-start",
+        justifyContent:"flex-end",
+        paddingLeft: 25
+    },
+    titles: {
+        fontSize: 30,
+        backgroundColor:"transparent",
+        color: "white",
+        marginBottom:10,
+        fontWeight: "300"
+    },
+    subtitle: {
+        fontSize:25,
+        backgroundColor:"transparent",
+        color: "white",
+        marginBottom:24
+    }
+})
